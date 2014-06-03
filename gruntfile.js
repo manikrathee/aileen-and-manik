c7284bd3fa0d3d180da519085f58951d3f915094
Manik Rathee <manikrathee@gmail.com>
2014-06-02 20:55:20 -0700

    imported framework, updated grunt, cimpiled and started running

---
+++ gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'css/style.css' : ['css/style.scss']
+        },
+        options: {
+          style: 'expanded'
+        },
+      },
+    },
+    watch: {
+      files: ['_prebuild/**'],
+      tasks: ['sass','concat'],
+    },
+    concat: {
+       options: {
+         separator: ';',
+       },
+       dist: {
+        src: ['js/libs/ss-social.js','js/libs/ss-symbolicons.js','js/libs/global.js'],
+        dest: 'js/script.js',
+        nonull: true,
+       },
+    },
+    cssmin: {
+      minify: {
+        src: ['css/style.css'],
+        dest: 'css/style.css',
+      }
+    },
+    uglify: {
+      my_target: {
+        files: {
+          'js/script.js' : ['js/script.js'],
+        }
+      }
+    },
+    jekyll: {
+      dist: {
+        options: {
+          config: '_config.yml'
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-jekyll');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default', ['sass','concat']);
+  grunt.registerTask('w', ['sass','concat','watch']);
+  grunt.registerTask('production', ['sass','concat','cssmin','uglify']);
+};
