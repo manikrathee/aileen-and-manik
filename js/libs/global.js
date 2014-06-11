37b6063d206cc985413ac19b65b9884e6f9c2cf9
Manik Rathee <manikrathee@gmail.com>
2014-06-11 16:52:10 -0700

    updated scripts for doc load

--- js/libs/global.js
+++ js/libs/global.js
@@ -5,42 +5,45 @@
  *
  */
 
-$('#sms input').change(function(e){
-  $('#phone').toggleClass('is-shown');
-  ga('send', 'event', 'signup-form', 'clicked-sms', 'displayed Phone Number Field');
-});
 
-$('.add-to-calendar').click(function(){
-  ga('send', 'event', 'Main', 'click', 'iCal Event');
 
-});
+$(document).ready(function() {
+  if ($(window).width() > 768) {
+    $('#mapbox').attr('height','500');
+  }
 
-$('.get-directions').click(function(e){
-  var elementOffset = $('#directions').offset().top;
+  $('#sms input').change(function(e){
+    $('#phone').toggleClass('is-shown');
+    ga('send', 'event', 'signup-form', 'clicked-sms', 'displayed Phone Number Field');
+  });
 
-  e.preventDefault();
+  $('.add-to-calendar').click(function(){
+    ga('send', 'event', 'Main', 'click', 'iCal Event');
+  });
 
-  $('html, body').animate({
-    scrollTop: elementOffset
-  }, 2000);
-  ga('send', 'event', 'Main', 'Click', 'Header Directions Link');
-});
+  $('.get-directions').click(function(e){
+    var elementOffset = $('#directions').offset().top;
 
-$('#directions').waypoint(function() {
-  ga('send', 'event', 'Main', 'Scrolled', 'Saw Wanderlust');
-  $('footer').addClass('is-shown');
-}, {
-  offset: function() {
-    return $(this).height();
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: elementOffset
+    }, 2000);
+    ga('send', 'event', 'Main', 'Click', 'Header Directions Link');
+  });
+
+  $('#directions').waypoint(function() {
+    ga('send', 'event', 'Main', 'Scrolled', 'Saw Wanderlust');
+    $('footer').addClass('is-shown');
+  }, {
+    offset: function() {
+      return $(this).height();
+    }
+  });
+
+  if (document.cookie.indexOf('emailSubmitted=') >= 0) {
+    $('#form').removeClass('is-shown');
+    $('#thank-you').addClass('is-shown');
+    ga('send', 'event', 'Main', 'Return Visit', 'viewed Form Thank You replacement');
   }
 });
-
-if (document.cookie.indexOf('emailSubmitted=') >= 0) {
-  $('#form').removeClass('is-shown');
-  $('#thank-you').addClass('is-shown');
-  ga('send', 'event', 'Main', 'Return Visit', 'viewed Form Thank You replacement');
-}
-
-if ($(window).width() > 768) {
-  $('#mapbox').attr('height','500');
-}
